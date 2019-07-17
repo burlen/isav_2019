164d1395319312754cfe45e3bea295c9beb2972b
Burlen Loring <bloring@lbl.gov>
2019-07-16 22:18:21 -0700

    add script to run all cases in 1 job

---
+++ submit_slice_knl_8192.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -l
+#SBATCH -N 243
+#SBATCH -C knl
+#SBATCH -t 04:00:00
+#SBATCH -q premium
+#SBATCH -A m3090
+#SBATCH -J in_transit_slice_knl
+#SBATCH -d singleton
+#SBATCH -o "in_transit_slice_knl-%j.out"
+
+set -x
+
+echo "============================================="
+echo "PlanarSlicePartitioner                       "
+echo "============================================="
+O=1 T=1 L=2048 M=8192 N=8192 ./in_transit_slice_knl.sh
+echo "============================================="
+O=1 T=1 L=2048 M=8192 N=4096 ./in_transit_slice_knl.sh
+echo "============================================="
+O=1 T=1 L=2048 M=8192 N=2048 ./in_transit_slice_knl.sh
+echo "============================================="
+O=1 T=1 L=2048 M=8192 N=1024 ./in_transit_slice_knl.sh
+echo "============================================="
+O=1 T=1 L=2048 M=8192 N=512  ./in_transit_slice_knl.sh
+echo "============================================="
+O=1 T=1 L=2048 M=8192 N=256  ./in_transit_slice_knl.sh
+echo "============================================="
+
+echo "#############################################"
+
+echo "============================================="
+echo "BlockPartitioner                             "
+echo "============================================="
+O=0 T=1 L=2048 M=8192 N=8192 ./in_transit_slice_knl.sh
+echo "============================================="
+O=0 T=1 L=2048 M=8192 N=4096 ./in_transit_slice_knl.sh
+echo "============================================="
+O=0 T=1 L=2048 M=8192 N=2048 ./in_transit_slice_knl.sh
+echo "============================================="
+O=0 T=1 L=2048 M=8192 N=1024 ./in_transit_slice_knl.sh
+echo "============================================="
+O=0 T=1 L=2048 M=8192 N=512  ./in_transit_slice_knl.sh
+echo "============================================="
+O=0 T=1 L=2048 M=8192 N=256  ./in_transit_slice_knl.sh
+echo "============================================="
