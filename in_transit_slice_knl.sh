307f6d66f982acc521c4a2f76b1f5e191e57af8f
Burlen Loring <bloring@lbl.gov>
2019-07-23 09:59:51 -0700

    don't use find to rm files, as it seems to cause job to hang

--- in_transit_slice_knl.sh
+++ in_transit_slice_knl.sh
@@ -62,7 +62,9 @@ blu=`echo -e '\e[36m'`
 wht=`echo -e '\e[0m'`
 
 #rm -rf ./slices_knl/*
-find ./slices_knl/ -exec rm {} \;
+#find ./slices_knl/ -exec rm {} \;
+rm -rf ./slices_knl
+mkdir ./slices_knl
 rm data_slice_knl.bp*
 
 module use /usr/common/software/sensei/modulefiles
--- submit_all_8192.sh
+++ submit_all_8192.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export ENABLE_TIMER=0
+export TIMER_ENABLE=0
 
 #sbatch submit_iso_has_8192.sh
-sbatch submit_slice_has_8192.sh
+#sbatch submit_slice_has_8192.sh
 #sbatch submit_iso_knl_8192.sh
 sbatch submit_slice_knl_8192.sh
