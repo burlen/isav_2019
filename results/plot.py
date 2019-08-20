8543f1a4da3354722f392a3a213d720172ee79ef
Burlen Loring <bloring@lbl.gov>
2019-08-20 11:01:42 -0700

    stuff after submission

--- results/plot.py
+++ results/plot.py
@@ -7,9 +7,10 @@ from scanf import scanf
 
 
 fni = sys.argv[1]
-title = sys.argv[2]
 fno = fni.replace('.csv', '.png')
-
+title = sys.argv[2]
+num_active_blocks = float(sys.argv[3])
+lna=1 # n act blk alpha
 
 f = open(fni, 'r')
 info = f.readline()
@@ -65,58 +66,58 @@ nopt_ii = np.where((opt == 0)  & (jnam == 'oscillator'))[0]
 opt_jj = np.where((opt == 1) & (jnam == 'ADIOS1EndPoint'))[0]
 nopt_jj = np.where((opt == 0)  & (jnam == 'ADIOS1EndPoint'))[0]
 
+## validation with oliver in gnumeric
+#sim_ranks = ncpu[opt_ii]
+#ept_ranks = ncpu[opt_jj]
+#
+#rt_opt = elapr[opt_ii]
+#rt_def = elapr[nopt_ii]
+#
+#cost_opt = elapr[opt_ii]*(ncpu[opt_ii] + ncpu[opt_jj])/3600.
+#cost_def = elapr[nopt_ii]*(ncpu[nopt_ii] + ncpu[nopt_jj])/3600.
+#
+#sys.stderr.write('sim_ranks, ept_ranks, rt_opt, rt_def, cost_opt, cost_def\n')
+#nn = len(sim_ranks)
+#ii = 0;
+#while ii  < nn:
+#    sys.stderr.write('%g, %g, %g, %g, %g, %g\n'%(sim_ranks[ii], \
+#        ept_ranks[ii], rt_opt[ii], rt_def[ii], cost_opt[ii], cost_def[ii]))
+#    ii += 1
 
 
 fig = plt.figure(figsize=(10,2.9))
+
+
+
+#fig = plt.figure()
 plt.subplot(1,4,1)
-
-plt.semilogx(ncpu[opt_jj], elapr[opt_ii], 'r-', marker='.', linewidth=2)
-plt.semilogx(ncpu[nopt_jj], elapr[nopt_ii], 'b--', marker='.', linewidth=2)
-plt.grid(True, which='both')
-#plt.grid(which='minor', linestyle=':')
-
-#plt.title('Iso-surface Haswell L=4096 M=8192')
-plt.xlabel('NCPU End-point', fontweight='bold')
-plt.ylabel('Time to solution (sec)', fontweight='bold')
-
-#fm  = pylab.get_current_fig_manager()
-#fm.window.wm_geometry("+500+0")
-
-
-
-#fig = plt.figure()
-plt.subplot(1,4,2)
-
-plt.semilogx(ncpu[opt_jj], elapr[opt_ii]*(ncpu[opt_ii] + ncpu[opt_jj])/3600., 'r-', marker='.' , linewidth=2)
-plt.semilogx(ncpu[nopt_jj], elapr[nopt_ii]*(ncpu[nopt_ii] + ncpu[nopt_jj])/3600., 'b--', marker='.', linewidth=2)
-plt.grid(True, which='both')
-
-#plt.title('Iso-surface Haswell L=4096 M=8192')
-plt.xlabel('NCPU End-point', fontweight='bold')
-plt.ylabel('Total cost (CPU Hours)', fontweight='bold')
-
-#fm  = pylab.get_current_fig_manager()
-#fm.window.wm_geometry("+500+0")
-
-
-
-
-
-#fig = plt.figure()
-plt.subplot(1,4,3)
 
 #plt.loglog(ncpu[opt_jj], averead[opt_jj], 'r.', linewidth=2)
 plt.loglog(ncpu[opt_jj], maxread[opt_jj], 'r-', marker='.', linewidth=2)
 #plt.loglog(ncpu[nopt_jj], averead[nopt_jj], 'b-.', linewidth=2)
 plt.loglog(ncpu[nopt_jj], maxread[nopt_jj], 'b--', marker='.', linewidth=2)
+
+mny, mxy = plt.ylim()
+plt.plot([num_active_blocks]*2, [mny, mxy], '-', color='orange', linewidth=2, alpha=lna, zorder=0)
+plt.ylim(mny, mxy)
+
 plt.grid(True, which='both')
 
 #plt.title('Iso-surface Haswell L=4096 M=8192')
-plt.xlabel('NCPU End-point', fontweight='bold')
-plt.ylabel('Max Read (MB) End-point', fontweight='bold')
+plt.xlabel('End-point ranks', fontweight='bold')
+plt.ylabel('End-point\nMax by rank read (MB)', fontweight='bold')
 
 #fm  = pylab.get_current_fig_manager()
 #fm.window.wm_geometry("+500+0")
+
+
+mxr_opt = maxread[opt_jj]
+mxr_nopt = maxread[nopt_jj]
+sys.stderr.write('data move ratio: %s\n'%(str(mxr_nopt/mxr_opt)))
+
+
+
+
 
 
 
@@ -129,7 +130,7 @@ plt.ylabel('Max Read (MB) End-point', fontweight='bold')
 #plt.grid(True, which='both')
 #
 #plt.title('Iso-surface Haswell L=4096 M=8192')
-#plt.xlabel('NCPU End-point')
+#plt.xlabel('End-point ranks')
 #plt.ylabel('Max Write (MB) Sim')
 #
 #fm  = pylab.get_current_fig_manager()
@@ -141,15 +142,86 @@ plt.ylabel('Max Read (MB) End-point', fontweight='bold')
 
 
 #fig = plt.figure()
-plt.subplot(1,4,4)
+plt.subplot(1,4,2)
 
 plt.loglog(ncpu[opt_jj], maxrss[opt_jj], 'r-', marker='.', linewidth=2)
 plt.loglog(ncpu[nopt_jj], maxrss[nopt_jj], 'b--', marker='.', linewidth=2)
+
+mny, mxy = plt.ylim()
+plt.plot([num_active_blocks]*2, [mny, mxy], '-', color='orange', linewidth=2, alpha=lna, zorder=0)
+plt.ylim(mny, mxy)
+
 plt.grid(True, which='both')
 
 #plt.title('Iso-surface Haswell L=4096 M=8192')
-plt.xlabel('NCPU End-point', fontweight='bold')
-plt.ylabel('Max RSS (KB) End-point', fontweight='bold')
+plt.xlabel('End-point ranks', fontweight='bold')
+plt.ylabel('End-point\nMax by rank RSS (KB)', fontweight='bold')
+
+
+sys.stderr.write('end-point ranks: %s\n'%(str(ncpu[opt_jj])))
+
+mxrss_opt = maxrss[opt_jj]
+mxrss_nopt = maxrss[nopt_jj]
+sys.stderr.write('rss ratio: %s\n'%(str(mxrss_nopt/mxrss_opt)))
+
+
+
+
+plt.subplot(1,4,3)
+
+plt.semilogx(ncpu[opt_jj], elapr[opt_ii], 'r-', marker='.', linewidth=2)
+plt.semilogx(ncpu[nopt_jj], elapr[nopt_ii], 'b--', marker='.', linewidth=2)
+
+mny, mxy = plt.ylim()
+plt.plot([num_active_blocks]*2, [mny, mxy], '-', color='orange', linewidth=2, alpha=lna, zorder=0)
+plt.ylim(mny, mxy)
+
+plt.grid(True, which='both')
+#plt.grid(which='minor', linestyle=':')
+
+#plt.title('Iso-surface Haswell L=4096 M=8192')
+plt.xlabel('End-point ranks', fontweight='bold')
+plt.ylabel('Time to solution (sec)', fontweight='bold')
+
+#fm  = pylab.get_current_fig_manager()
+#fm.window.wm_geometry("+500+0")
+
+elapr_opt = maxread[opt_ii]
+elapr_nopt = maxread[nopt_ii]
+sys.stderr.write('elapsed time ratio: %s\n'%(str(elapr_nopt/elapr_opt)))
+
+
+#fig = plt.figure()
+plt.subplot(1,4,4)
+
+
+cos_opt = elapr[opt_ii]*(ncpu[opt_ii] + ncpu[opt_jj])/3600.
+cos_nopt = elapr[nopt_ii]*(ncpu[nopt_ii] + ncpu[nopt_jj])/3600.
+
+
+plt.semilogx(ncpu[opt_jj], elapr[opt_ii]*(ncpu[opt_ii] + ncpu[opt_jj])/3600., 'r-', marker='.' , linewidth=2)
+plt.semilogx(ncpu[nopt_jj], elapr[nopt_ii]*(ncpu[nopt_ii] + ncpu[nopt_jj])/3600., 'b--', marker='.', linewidth=2)
+
+mny, mxy = plt.ylim()
+plt.plot([num_active_blocks]*2, [mny, mxy], '-', color='orange', linewidth=2, alpha=lna, zorder=0)
+plt.ylim(mny, mxy)
+
+plt.grid(True, which='both')
+
+#plt.title('Iso-surface Haswell L=4096 M=8192')
+plt.xlabel('End-point ranks', fontweight='bold')
+plt.ylabel('Total cost (CPU Hours)', fontweight='bold')
+
+#fm  = pylab.get_current_fig_manager()
+#fm.window.wm_geometry("+500+0")
+
+mxr_opt = maxread[opt_jj]
+mxr_nopt = maxread[nopt_jj]
+sys.stderr.write('solution cost ratio: %s\n'%(str(mxr_nopt/mxr_opt)))
+
+
+
+
 
 plt.legend(['opt. part','default']) #,loc='upper left', bbox_to_anchor=(1,1))
 
@@ -174,7 +246,7 @@ plt.show()
 #plt.grid(True, which='both')
 #
 #plt.title('Iso-surface Haswell L=4096 M=8192')
-#plt.xlabel('NCPU End-point')
+#plt.xlabel('End-point ranks')
 #plt.ylabel('Max RSS Sim (KB)')
 #
 #fm  = pylab.get_current_fig_manager()
@@ -191,7 +263,36 @@ plt.show()
 
 
 
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Make data.
+X = elapr_nopt
+
+xx = elapr_nopt*(ncpu[opt_ii] + ncpu[opt_jj])/3600.
+Y = mxr_nopt
+X, Y = np.meshgrid(X, Y)
+Q = X*xx
+Z = Q/np.max(Q) + Y/np.max(Y)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
 
 
 
--- results/plot_results_run_3.sh
+++ results/plot_results_run_3.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-python plot.py run_3/in_transit_iso_has-23150227.csv 'Iso-surface Haswell L=4096 M=8192'
-python plot.py run_3/in_transit_iso_knl-23150244.csv 'Iso-surface KNL L=2048 M=8192'
+# ave num active blocks from part
+#nab_iso=1857.3 #ave
+nab_iso=2018.5 # median
+nab_slice=830
 
-python plot.py run_3/in_transit_slice_has-23150232.csv 'Slice Haswell L=4096 M=8192'
-python plot.py run_3/in_transit_slice_knl-23150248.csv 'Slice KNL L=2048 M=8192'
+python plot.py run_3/in_transit_iso_has-23150227.csv 'Iso-surface Haswell. ${\bf 4096^3}$ cells, 8192 blocks, M=8192, N=128..8192' ${nab_iso}
+python plot.py run_3/in_transit_iso_knl-23150244.csv 'Iso-surface KNL. ${\bf 2048^3}$ cells, 8192 blocks, M=8192, N=128..8192' ${nab_iso}
 
+python plot.py run_3/in_transit_slice_has-23150232.csv 'Slice Haswell. ${\bf 4096^3}$ cells, 8192 blocks, M=8192, N=128..8192' ${nab_slice}
+python plot.py run_3/in_transit_slice_knl-23150248.csv 'Slice KNL. ${\bf 2048^3}$ cells, 8192 blocks, M=8192, N=128..8192' ${nab_slice}
 
+python plot.py run_3/in_transit_slice_has-23240948.csv 'Slice Haswell. ${\bf 4096^3}$ cells, 8192 blocks, M=8192, N=128..8192' ${nab_slice}
+#python plot.py run_3/in_transit_slice_knl-.csv 'Slice KNL L=2048 M=8192'
